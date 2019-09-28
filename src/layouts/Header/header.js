bae412fc234a25a487aaf3328341a4fed2ade541
andy00614 <33732843@qq.com>
2019-09-28 16:55:36 +0800

    initial

---
+++ src/layouts/Header/header.js
@@ -0,0 +1,11 @@
+import styles from "./user.scss";
+
+function Header() {
+  return (
+    <div className={styles.header}>
+      Header
+    </div>
+  )
+}
+
+export default Header
---
+++ src/layouts/index.js
@@ -0,0 +1,13 @@
+import styles from './index.css';
+import Header from './Header/header'
+
+function BasicLayout(props) {
+  return (
+    <div className={styles.normal}>
+      <Header></Header>
+      {props.children}
+    </div>
+  );
+}
+
+export default BasicLayout;
